f3dd988b903df2257cf1f37e828a867d677b9447
Luciano Pereira <lucianop27@gmail.com>
2026-09-16 15:27:49 -0300

    removendo outputs.tf de prd.

--- ambiente/prd/vm_linux/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vm_public_ips" {
-  description = "IPs Publicos das maquinas virtuais"
-  value = {
-    "vm-linux-01" = azurerm_public_ip.pip_vm_linux_1.ip_address
-    "vm-linux-02" = azurerm_public_ip.pip_vm_linux_2.ip_address
-    "vm-linux-03" = azurerm_public_ip.pip_vm_linux_3.ip_address
-  }
-}
--- ambiente/prd/vm_windows/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "vm_public_ips" {
-  description = "IP Publico da VM Windows"
-  value = {
-    "vm-win-01" = azurerm_public_ip.pip_vm_win_1.ip_address
-  }
-}
