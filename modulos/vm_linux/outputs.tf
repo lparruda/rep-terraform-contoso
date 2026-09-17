2315153aa18c8b76700cfcac1ef77f23a18cf783
Luciano Pereira <lucianop27@gmail.com>
2026-09-17 10:08:48 -0300

    ajustando o outputs.tf.

--- modulos/vm_linux/outputs.tf
+++ modulos/vm_linux/outputs.tf
@@ -1,3 +1,9 @@
 output "nic_ids" {
   value = { for k, v in azurerm_network_interface.example : k => v.id }
 }
+
+output "vm_id" {
+  description = "ID da Virtual Machine Linux"
+  value       = azurerm_linux_virtual_machine.example.id
+}
+
